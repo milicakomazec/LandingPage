784b1295f0b233da3b34f8235e749fdf74a60478
milicakomazec <milicakomazec.it@gmail.com>
2022-07-06 16:27:56 +0200

    Add footer component.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <ul className="footer-list">
+        <li>Privacy</li>
+        <li>Terms</li>
+        <li>Contact</li>
+      </ul>
+      <div className="copyright">Copyright 2016, Original Trombones</div>
+    </div>
+  );
+};
+
+export default Footer;
